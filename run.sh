637c2ab9f75ecef5b35a906a86ec9dea2528d2e9
Aleksei Balandin <alexbalandi@gmail.com>
2023-09-26 20:46:02 +0300

    collect static

--- run.sh
+++ run.sh
@@ -6,4 +6,5 @@ if [[ "$SKIP_DB_UPDATE" != "True" ]]; then
     python3 manage.py import_heroes
     python3 manage.py clear_cache
 fi
+python manage.py collectstatic
 gunicorn config.wsgi:application --bind 0.0.0.0:$PORT
