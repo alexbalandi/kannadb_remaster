96ea32b4c373efa18ba69c52336c590bcdd49658
Aleksei Balandin <alexbalandi@gmail.com>
2023-09-26 21:12:35 +0300

    fix allowed hosts

--- config/settings/production.py
+++ config/settings/production.py
@@ -7,7 +7,7 @@ import sys
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['linuskannadb-production.up.railway.app/'])
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['linuskannadb-production.up.railway.app'])
 
 # DATABASES
 # ------------------------------------------------------------------------------
