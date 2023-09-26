17e7669fa4d05eef12267c24c8d2d6ea3627dc5f
Aleksei Balandin <alexbalandi@gmail.com>
2023-09-26 20:23:57 +0300

    add skip db populate

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 python3 manage.py makemigrations
 python3 manage.py migrate
-python3 manage.py curl_heroes
-python3 manage.py import_heroes
-python3 manage.py clear_cache
+if [[ "$SKIP_DB_UPDATE" != "True" ]]; then
+    python3 manage.py curl_heroes
+    python3 manage.py import_heroes
+    python3 manage.py clear_cache
+fi
 gunicorn config.wsgi:application --bind 0.0.0.0:$PORT
