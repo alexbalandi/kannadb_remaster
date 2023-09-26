b17a737af939548d436d669dee4d4a5916f4db3a
Aleksei Balandin <alexbalandi@gmail.com>
2023-09-26 17:10:10 +0300

    hax to go to new ver

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.conf.urls import url
+from django.urls import re_path as url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
