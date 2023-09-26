a62499b96e6ad799862b838aa770b71ce701b7b2
Aleksei Balandin <alexbalandi@gmail.com>
2023-09-26 18:52:55 +0300

    switch cache to dummy, bind port

--- config/settings/production.py
+++ config/settings/production.py
@@ -17,16 +17,22 @@ DATABASES['default']['CONN_MAX_AGE'] = env.int('CONN_MAX_AGE', default=60)  # no
 
 # CACHES
 # ------------------------------------------------------------------------------
+# CACHES = {
+#     'default': {
+#         'BACKEND': 'django_redis.cache.RedisCache',
+#         'LOCATION': env('REDIS_URL'),
+#         'OPTIONS': {
+#             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+#             # Mimicing memcache behavior.
+#             # http://niwinz.github.io/django-redis/latest/#_memcached_exceptions_behavior
+#             'IGNORE_EXCEPTIONS': True,
+#         }
+#     }
+# } # TODO: decide on efficiency here
 CACHES = {
     'default': {
-        'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': env('REDIS_URL'),
-        'OPTIONS': {
-            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-            # Mimicing memcache behavior.
-            # http://niwinz.github.io/django-redis/latest/#_memcached_exceptions_behavior
-            'IGNORE_EXCEPTIONS': True,
-        }
+        'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
+        'LOCATION': 'your_app_name_cache',
     }
 }
 
--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ python3 manage.py migrate
 python3 manage.py curl_heroes
 python3 manage.py import_heroes
 python3 manage.py clear_cache
-gunicorn config.wsgi:application
+gunicorn config.wsgi:application --bind 0.0.0.0:$PORT
