126f4166188f6c185877622a76890185f1795fdb
Aleksei Balandin <alexbalandi@gmail.com>
2023-09-27 09:18:40 +0300

    new allowe host

--- config/settings/production.py
+++ config/settings/production.py
@@ -8,7 +8,7 @@ import sys
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list(
-    "DJANGO_ALLOWED_HOSTS", default=["linuskannadb-production.up.railway.app"]
+    "DJANGO_ALLOWED_HOSTS", default=["kannadb.up.railway.app"]
 )
 
 # DATABASES
