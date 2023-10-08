1f2b22eba69022abb62a7a6753ab6478f7642ffa
Aleksei Balandin <alexbalandi@gmail.com>
2023-10-08 08:59:50 +0300

    add toggle

--- linus/static/js/dark_reader.js
+++ linus/static/js/dark_reader.js
@@ -8,3 +8,20 @@ if (
     sepia: 10,
   });
 }
+
+const toggleButton = document.getElementById("darkModeToggle");
+const body = document.body;
+
+toggleButton.addEventListener("click", () => {
+  if (body.classList.contains("dark-mode")) {
+    body.classList.remove("dark-mode");
+    DarkReader.disable();
+  } else {
+    body.classList.add("dark-mode");
+    DarkReader.enable({
+      brightness: 100,
+      contrast: 90,
+      sepia: 10,
+    });
+  }
+});
