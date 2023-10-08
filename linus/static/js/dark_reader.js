40d691d48db01fb3d3db79fb72eee1c64039caa1
Aleksei Balandin <alexbalandi@gmail.com>
2023-10-08 09:09:20 +0300

    wait for dom load

--- linus/static/js/dark_reader.js
+++ linus/static/js/dark_reader.js
@@ -1,27 +1,31 @@
-if (
-  window.matchMedia &&
-  window.matchMedia("(prefers-color-scheme: dark)").matches
-) {
-  DarkReader.enable({
-    brightness: 100,
-    contrast: 90,
-    sepia: 10,
-  });
-}
-
-const toggleButton = document.getElementById("darkModeToggle");
-const body = document.body;
-
-toggleButton.addEventListener("click", () => {
-  if (body.classList.contains("dark-mode")) {
-    body.classList.remove("dark-mode");
-    DarkReader.disable();
-  } else {
-    body.classList.add("dark-mode");
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleButton = document.getElementById("darkModeToggle");
+  const body = document.body;
+  if (
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+  ) {
     DarkReader.enable({
       brightness: 100,
       contrast: 90,
       sepia: 10,
+    });
+  }
+
+  if (toggleButton) {
+    // Check if the button exists
+    toggleButton.addEventListener("click", () => {
+      if (body.classList.contains("dark-mode")) {
+        body.classList.remove("dark-mode");
+        DarkReader.disable();
+      } else {
+        body.classList.add("dark-mode");
+        DarkReader.enable({
+          brightness: 100,
+          contrast: 90,
+          sepia: 10,
+        });
+      }
     });
   }
 });
