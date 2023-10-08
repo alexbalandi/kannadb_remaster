54f8c486cd52a1005c0febfeddfc856eae4c6746
Aleksei Balandin <alexbalandi@gmail.com>
2023-10-08 09:16:44 +0300

    smurter dark mode

--- linus/static/js/dark_reader.js
+++ linus/static/js/dark_reader.js
@@ -1,30 +1,44 @@
 document.addEventListener("DOMContentLoaded", function () {
   const toggleButton = document.getElementById("darkModeToggle");
   const body = document.body;
-  if (
-    window.matchMedia &&
-    window.matchMedia("(prefers-color-scheme: dark)").matches
-  ) {
+
+  function enableDarkMode() {
+    body.classList.add("dark-mode");
     DarkReader.enable({
       brightness: 100,
       contrast: 90,
       sepia: 10,
     });
+    localStorage.setItem("darkMode", "enabled");
+  }
+
+  function disableDarkMode() {
+    body.classList.remove("dark-mode");
+    DarkReader.disable();
+    localStorage.setItem("darkMode", "disabled");
+  }
+
+  // Check the state flag in local storage on initial load
+  if (localStorage.getItem("darkMode") === "enabled") {
+    enableDarkMode();
+  } else if (
+    localStorage.getItem("darkMode") === null &&
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+  ) {
+    // If no state flag in local storage, check for the OS preference
+    enableDarkMode();
+  } else {
+    disableDarkMode();
   }
 
   if (toggleButton) {
-    // Check if the button exists
+    // Handle the button click event
     toggleButton.addEventListener("click", () => {
       if (body.classList.contains("dark-mode")) {
-        body.classList.remove("dark-mode");
-        DarkReader.disable();
+        disableDarkMode();
       } else {
-        body.classList.add("dark-mode");
-        DarkReader.enable({
-          brightness: 100,
-          contrast: 90,
-          sepia: 10,
-        });
+        enableDarkMode();
       }
     });
   }
